7e2a0fb0631a250d890039e2011d9dd4ce8f8028
Amanda G <amagonzalez27@gmail.com>
2019-01-29 16:48:41 -0500

    add: signout button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 
+import Navbar from './components/Navbar';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Question from './components/Question';
@@ -12,19 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav>
-          <ul className="navbar-list">
-            <Link className="navbar-item active" to={'/'}>
-              Home
-            </Link>
-            <Link className="navbar-item" to={'/login'}>
-              Login
-            </Link>
-            <Link className="navbar-item" to={'/signup'}>
-              SignUp
-            </Link>
-          </ul>
-        </nav>
+        <Navbar />
         <div className="App-main ">
           <Switch>
             <Route exact path="/" component={Home} />
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import ThunkMiddleware from 'redux-thunk';
-import { database } from '../configs/firebase_init';
+import { database, auth } from '../configs/firebase_init';
 
 const GET_CURRENT_LEVEL_QUESTIONS = 'GET_CURRENT_LEVEL_QUESTIONS';
 const GET_CURRENT_USER = 'GET_CURRENT_USER';
+const LOG_OUT_USER = 'LOG_OUT_USER';
 
 export const getLevelQuestions = level => ({
   type: GET_CURRENT_LEVEL_QUESTIONS,
@@ -12,6 +13,10 @@ export const getLevelQuestions = level => ({
 export const getCurrentUser = uid => ({
   type: GET_CURRENT_USER,
   uid,
+});
+
+export const logOutUser = () => ({
+  type: LOG_OUT_USER,
 });
 
 //THUNK
@@ -54,6 +59,8 @@ const user = (state = initialState, action) => {
       return { ...state, currentLevelQuestions: action.level };
     case GET_CURRENT_USER:
       return { ...state, currentUser: action.uid };
+    case LOG_OUT_USER:
+      return { ...state, currentUser: '' };
     default:
       return state;
   }
