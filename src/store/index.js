dcf66bdad9009d16eda8224fc89b1247545673ad
rhianna larocque <rhiannamrl@gmail.com>
2019-01-29 12:14:36 -0500

    refactor: converted data obj to array and updated obj.values

--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,39 @@
-import { createStore, applyMiddleware } from 'redux';
-import ThunkMiddleware from 'redux-thunk';
-import { database } from '../configs/firebase_init';
+import { createStore, applyMiddleware } from 'redux'
+import ThunkMiddleware from 'redux-thunk'
+import { database } from '../configs/firebase_init'
 
-const GET_CURRENT_LEVEL_QUESTIONS = 'GET_CURRENT_LEVEL_QUESTIONS';
+const GET_CURRENT_LEVEL_QUESTIONS = 'GET_CURRENT_LEVEL_QUESTIONS'
 
 export const getLevelQuestions = level => ({
   type: GET_CURRENT_LEVEL_QUESTIONS,
-  level,
-});
+  level
+})
 
 //THUNK
 export const fetchLevelQuestions = levelId => {
-  console.log(levelId);
   return async dispatch => {
     try {
       database
         .ref('levels/')
         .child(levelId)
         .once('value', snapshot => {
-          console.log('hello');
-          const exists = snapshot.val() !== null;
+          const exists = snapshot.val() !== null
           if (exists) {
-            let data = snapshot.val();
-            //   let results = Object.keys(data).map(key => data[key])
-            const result = Object.keys(data).map(el => data[el]);
-            console.log('DATA TEST QS LVL 1', result);
-            dispatch(getLevelQuestions(data));
+            let data = snapshot.val()
+            const result = Object.keys(data).map(el => data[el])
+            dispatch(getLevelQuestions(result))
           }
         })
-        .catch(error => console.log(error));
+        .catch(error => console.log(error))
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
-};
+  }
+}
 
 const initialState = {
-  currentLevelQuestions: {},
-};
+  currentLevelQuestions: {}
+}
 
 /**
  * REDUCER
@@ -46,10 +42,10 @@ const initialState = {
 const user = (state = initialState, action) => {
   switch (action.type) {
     case GET_CURRENT_LEVEL_QUESTIONS:
-      return { ...state, currentLevelQuestions: action.level };
+      return { ...state, currentLevelQuestions: action.level }
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default createStore(user, applyMiddleware(ThunkMiddleware));
+export default createStore(user, applyMiddleware(ThunkMiddleware))
