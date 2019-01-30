7dc58ab17e66f5b6e82326b508ab97150fb53c41
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-30 15:07:30 -0500

    will I get the same build fail with travis now?

---
+++ src/configs/firebase_config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+};
