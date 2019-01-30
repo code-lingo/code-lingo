e2ee16e04beb238a0b62cd08c51a6e56375354db
rhianna larocque <rhiannamrl@gmail.com>
2019-01-30 16:28:39 -0500

    feat: results component

--- src/components/Level.js
+++ src/components/Level.js
@@ -1,40 +1,33 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchLevelQuestions } from '../store';
-import Question from './Question';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchLevelQuestions } from '../store'
+import Question from './Question'
 
 class Level extends Component {
-
   componentDidMount() {
-    this.props.getQuestions('level1');
+    this.props.getQuestions('level1')
   }
-
-  render () {
-    // console.log("PROPS", this.props)
-
+  render() {
     if (!this.props) {
       return null
     }
-
-    return (
-      <Question questions={this.props.questions}/>)
+    return <Question questions={this.props.questions} />
   }
-
 }
 
 const mapStateToProps = state => {
   return {
-    questions: state.currentLevelQuestions,
-  };
-};
+    questions: state.currentLevelQuestions
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
-    getQuestions: level => dispatch(fetchLevelQuestions(level)),
-  };
-};
+    getQuestions: level => dispatch(fetchLevelQuestions(level))
+  }
+}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Level);
+)(Level)
---
+++ src/components/Results.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//calc the score of the first level
+//if the score is <= 80% continue to next level or replay
+//else replay or exit (to home page)
+
+const Results = props => {
+  return (
+    <div>
+      <h2>Great Job!</h2>
+      <h3>Your score is 4/5</h3>
+    </div>
+  )
+}
+
+export default Results
