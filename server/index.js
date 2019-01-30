7085c4af4b4bce01d5ddcf40392058bc59d52da9
McRae Petrey <mcrae@McRaes-MacBook-Pro-2018.local>
2019-01-30 15:55:46 -0500

    travis please?

--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ firebase.initializeApp({
   databaseURL: 'https://codelingo-99ba7.firebaseio.com',
 });
 
+/**
+ * In your development environment, you can keep all of your
+ * app's secret API keys in a file called `secrets.js`, in your project
+ * root. This file is included in the .gitignore - it will NOT be tracked
+ * or show up on Github. On your production server, you can add these
+ * keys as environment variables, so that they can still be read by the
+ * Node process on process.env
+ */
+if (process.env.NODE_ENV !== 'production') require('../secrets');
+
 // TODO: move these to firebase endpoints?
 
 // const db = firebase.database();
