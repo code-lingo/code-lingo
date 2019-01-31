ef7a2d8f384daf3d1eec89a419886c0896e5d84d
rhianna larocque <rhiannamrl@gmail.com>
2019-01-31 17:56:08 -0500

    trying hard

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,31 @@
-import { createStore, applyMiddleware } from 'redux';
-import ThunkMiddleware from 'redux-thunk';
-import loggingMiddleware from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux'
+import ThunkMiddleware from 'redux-thunk'
+import loggingMiddleware from 'redux-logger'
 
-import { database } from '../configs/firebase_init';
+import { database } from '../configs/firebase_init'
 
-export const GET_CURRENT_LEVEL_QUESTIONS = 'GET_CURRENT_LEVEL_QUESTIONS';
-const GET_CURRENT_USER = 'GET_CURRENT_USER';
-const LOG_OUT_USER = 'LOG_OUT_USER';
+export const GET_CURRENT_LEVEL_QUESTIONS = 'GET_CURRENT_LEVEL_QUESTIONS'
+export const ADD_SCORE_TO_LEADERBOARD = 'ADD_SCORE_TO_LEADERBOARD'
+const GET_CURRENT_USER = 'GET_CURRENT_USER'
+const LOG_OUT_USER = 'LOG_OUT_USER'
 
 export const getLevelQuestions = questions => ({
   type: GET_CURRENT_LEVEL_QUESTIONS,
-  questions,
-});
+  questions
+})
 export const getCurrentUser = uid => ({
   type: GET_CURRENT_USER,
-  uid,
-});
+  uid
+})
 
 export const logOutUser = () => ({
-  type: LOG_OUT_USER,
-});
+  type: LOG_OUT_USER
+})
+
+export const addScoreLeaderBoard = score => ({
+  type: ADD_SCORE_TO_LEADERBOARD,
+  score
+})
 
 //THUNK
 export const fetchLevelQuestions = levelId => {
@@ -29,26 +35,50 @@ export const fetchLevelQuestions = levelId => {
         .ref('levels/')
         .child(levelId)
         .once('value', snapshot => {
-          const exists = snapshot.val() !== null;
+          const exists = snapshot.val() !== null
           if (exists) {
-            let data = snapshot.val();
+            let data = snapshot.val()
             const result = Object.keys(data)
               .map(el => data[el])
-              .sort((a, b) => a.id - b.id);
-            dispatch(getLevelQuestions(result));
+              .sort((a, b) => a.id - b.id)
+            dispatch(getLevelQuestions(result))
           }
         })
-        .catch(error => console.log(error));
+        .catch(error => console.log(error))
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
-};
+  }
+}
+
+export const addLeaderboardScore = (userId, score) => {
+  return async dispatch => {
+    try {
+      database.currentUser
+        .ref('leaderboard/')
+        .child(userId)
+        .set({
+          score: score
+        })
+        .once('value', snapshot => {
+          const exists = snapshot.val() !== null
+          if (exists) {
+            let data = snapshot.val()
+            dispatch(addScoreLeaderBoard(data))
+          }
+        })
+        .catch(error => console.log(error))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
 
 const initialState = {
   currentLevelQuestions: [],
   currentUser: '',
-};
+  userScore: 0
+}
 
 /**
  * REDUCER
@@ -57,17 +87,19 @@ const initialState = {
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CURRENT_LEVEL_QUESTIONS:
-      return { ...state, currentLevelQuestions: action.questions };
+      return { ...state, currentLevelQuestions: action.questions }
     case GET_CURRENT_USER:
-      return { ...state, currentUser: action.uid };
+      return { ...state, currentUser: action.uid }
     case LOG_OUT_USER:
-      return { ...state, currentUser: '' };
+      return { ...state, currentUser: '' }
+    case ADD_SCORE_TO_LEADERBOARD:
+      return { ...state, score: action.score }
     default:
-      return state;
+      return state
   }
-};
+}
 
 export default createStore(
   reducer,
   applyMiddleware(ThunkMiddleware, loggingMiddleware)
-);
+)
