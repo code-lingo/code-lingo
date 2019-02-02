c7559684ca30c839647dc3c3e3c465952debc4a1
Amanda G <amagonzalez27@gmail.com>
2019-02-02 11:20:58 -0500

    live leaderboard renders top programmers

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { fetchLeaderBoard } from '../store';
+import { connect } from 'react-redux';
+
+class Leaderboard extends Component {
+  componentDidMount() {
+    this.props.fetchLeaderBoard(this.props.currentUser);
+  }
+  render() {
+    if (!this.props.leaderboard.length) {
+      return null;
+    }
+    return (
+      <div>
+        <h1>Leaderboard</h1>
+        <table>
+          <tr>
+            <th>Programmers</th>
+            <th>Rank</th>
+          </tr>
+          {this.props.leaderboard.map(leader => (
+            <tr>
+              <td>{leader.username}</td>
+              <td>{leader.score}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapState = state => ({
+  currentUser: state.currentUser,
+  leaderboard: state.leaderboard,
+});
+
+const mapDispatch = {
+  fetchLeaderBoard,
+};
+
+export default connect(
+  mapState,
+  mapDispatch
+)(Leaderboard);
