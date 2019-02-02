b838520a3eded5bcb5f374d17a6657d25967af84
Camryn Pearson <camrynpearson@gmail.com>
2019-02-02 16:52:31 -0500

    styled nav

--- src/components/Home/SidePanel.js
+++ src/components/Home/SidePanel.js
@@ -1,22 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from 'react'
 
 //calc the score of the first level
 //if the score is <= 80% continue to next level or replay
 //else replay or exit (to home page)
 
 export const SidePanel = () => {
-  const level = 'Level 2';
-  const score = 100;
+  const level = 'Level 2'
+  const score = 100
 
   return (
-    <Fragment>
-      <div id="side-panel">
-        <div>Current Level: {level}</div>
-        <div>Total Score: {score}</div>
+    <div>
+      <div id='side-panel'>
+        <h3>Current Level:</h3>
+        <p id='side-panel-level'>{level}</p>
+        <h3>Total Score:</h3>
+        <p>{score}</p>
       </div>
-      <div id="leader-board">
+      {/* <div id="leader-board">
         <p>Leaderboard coming!</p>
-      </div>
-    </Fragment>
-  );
-};
+      </div> */}
+    </div>
+  )
+}
