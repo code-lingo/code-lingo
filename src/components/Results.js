4b0a5d16cc55b4194550dc1579502f926f737172
rhianna larocque <rhiannamrl@gmail.com>
2019-02-03 18:30:59 -0500

    update

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addLeaderboardScore } from '../store/reducers/userScore'
-import { Link } from 'react-router-dom'
 
 class Results extends Component {
   componentDidMount() {
@@ -13,22 +12,12 @@ class Results extends Component {
 
   render() {
     const { totalAnswers, correctAnswers } = this.props
-    console.log(this.props)
     return (
       <div className="card question">
         <h2>Great Job!</h2>
         <h3>
           Your score is {correctAnswers.length}/{totalAnswers.length}
         </h3>
-        <h4>
-          <Link to={'/'}>Home</Link>
-        </h4>
-        {/* <h4>
-          <Link>Try Again</Link>
-        </h4>
-        <h4>
-          <Link>Next Level</Link>
-        </h4> */}
       </div>
     )
   }
