2749c6ece7c4b89e1a74e0475301ea824f85ca3b
Amanda G <amagonzalez27@gmail.com>
2019-02-02 19:16:02 -0500

    1/2 test for navbar.spec.js created

--- src/components/Navbar.spec.js
+++ src/components/Navbar.spec.js
@@ -1,22 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../App';
+import { Navbar } from './Navbar';
+import { Provider } from 'react-redux';
+import store from '../store';
+import {
+  BrowserRouter as Router,
+  MemoryRouter,
+  NavLink,
+} from 'react-router-dom';
+// Testing packages
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import sinon from 'sinon';
-import chai, { expect } from 'chai';
+import sinon, { spy, mock } from 'sinon';
+import chai, { expect, assert } from 'chai';
 import sinonChai from 'sinon-chai';
-
+// Test set-up
 chai.use(sinonChai);
 configure({ adapter: new Adapter() });
 sinon.assert.expose(chai.assert, { prefix: '' });
 
-function handleSignOut(callback) {
-  callback('SUCCESSFULLY LOGGED OUT USER!');
-}
-
 describe('/Navbar', () => {
-  it('contains a list of navigation tabs', () => {
-    // TODO: finish navbar tests
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  it('renders Navbar correctly', () => {
+    expect(
+      wrapper.contains(
+        <NavLink className="navbar-item active" to={'/'}>
+          Home
+        </NavLink>
+      )
+    ).to.equal(true);
+    expect(
+      wrapper.contains(
+        <NavLink className="navbar-item" to={'/login'}>
+          Login
+        </NavLink>
+      )
+    ).to.equal(true);
+    expect(
+      wrapper.contains(
+        <NavLink className="navbar-item" to={'/signup'}>
+          SignUp
+        </NavLink>
+      )
+    ).to.equal(true);
+  });
+
+  xit('fires singout button', () => {
+    const navbarComponent = shallow(<Navbar />);
+    const instance = navbarComponent.instance();
+    const handleSignOutSpy = sinon.spy(instance, 'handleSignOut');
+    instance.forceUpdate();
+    navbarComponent.find('form').simulate('submit');
+    sinon.assert.calledOnce(handleSignOutSpy);
   });
 });
