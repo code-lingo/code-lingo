9315d04356fe7c3da9f0e8ab226a54cde9b780eb
rhianna larocque <rhiannamrl@gmail.com>
2019-02-03 18:00:29 -0500

    idk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,30 +16,30 @@ export class Navbar extends Component {
   render() {
     return (
       <nav>
-        <div id='title'>
+        <div id="title">
           <h1>Codelingo</h1>
         </div>
         {!this.props.currentUser ? (
           <div>
-            <NavLink className='navbar-item active' to={'/'}>
+            <NavLink className="navbar-item active" to={'/'}>
               <h2>Home</h2>
             </NavLink>
-            <NavLink className='navbar-item' to={'/login'}>
+            <NavLink className="navbar-item" to={'/login'}>
               <h2>Login</h2>
             </NavLink>
-            <NavLink className='navbar-item' to={'/signup'}>
+            <NavLink className="navbar-item" to={'/signup'}>
               <h2>SignUp</h2>
             </NavLink>
           </div>
         ) : (
           <div>
-            <NavLink className='navbar-item active' to={'/'}>
+            <NavLink className="navbar-item active" to={'/'}>
               <h2>Home</h2>
             </NavLink>
-            <NavLink className='navbar-item' to={'/leaderboard'}>
+            <NavLink className="navbar-item" to={'/leaderboard'}>
               <h2>Leaderboard</h2>
             </NavLink>
-            <NavLink to='/login' className='navbar-item'>
+            <NavLink to="/login" className="navbar-item">
               <h2 onClick={this.handleSignOut.bind(this)}>SignOut</h2>
             </NavLink>
           </div>
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,39 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addLeaderboardScore } from '../store';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addLeaderboardScore } from '../store'
+import { Link } from 'react-router-dom'
 
 class Results extends Component {
   componentDidMount() {
     this.props.addLeaderboardScore(
       this.props.currentUser,
-      this.props.correctAnswers.length,
-      1
-    );
+      this.props.correctAnswers.length
+    )
   }
 
   render() {
-    const { totalAnswers, correctAnswers } = this.props;
+    const { totalAnswers, correctAnswers } = this.props
+    console.log('LEVEL ID', this.props.match.params.levelId)
     return (
-      <div className='card question'>
+      <div className="card question">
         <h2>Great Job!</h2>
         <h3>
           Your score is {correctAnswers.length}/{totalAnswers.length}
         </h3>
+        <h4>
+          <Link to={'/'}>Home</Link>
+        </h4>
+        {/* <h4>
+          <Link>Try Again</Link>
+        </h4>
+        <h4>
+          <Link>Next Level</Link>
+        </h4> */}
       </div>
-    );
+    )
   }
 }
 
-const mapState = state => ({
+const mapState = (state, { match }) => ({
   currentUser: state.currentUser,
-  currentLevel: state.currentLevel,
-});
+  currentLevel: state.currentLevel
+})
 
 const mapDispatch = {
-  addLeaderboardScore,
-};
+  addLeaderboardScore
+}
 
 export default connect(
   mapState,
   mapDispatch
-)(Results);
+)(Results)
