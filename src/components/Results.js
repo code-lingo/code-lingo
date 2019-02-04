cac9919f517906d748af4b9728b062863152919b
Amanda G <amagonzalez27@gmail.com>
2019-02-04 16:44:37 -0500

    update result buttons

--- src/components/Results.js
+++ src/components/Results.js
@@ -20,19 +20,17 @@ class Results extends Component {
     return (
       <div className="card question">
         <h3>Your score is {score}</h3>
+        <div className="border-top" />
         {percentage < 0.75 ? (
           <div>
-            <Link to={`/`}>
-              <h3>Try Again!</h3>
+            <Link className="question-submit" to={`/`}>
+              <button type="submit">Try Again!</button>
             </Link>
           </div>
         ) : (
-          <div>
-            <div className="border-top" />
-            <Link className="question-submit" to={`/`}>
-              <button type="submit">You're on a roll!</button>
-            </Link>
-          </div>
+          <Link className="question-submit" to={`/`}>
+            <button type="submit">You're on a roll!</button>
+          </Link>
         )}
       </div>
     );
