2217629a8ca1d07d4310accccdcff02a274c0ff6
Amanda G <amagonzalez27@gmail.com>
2019-02-04 16:07:37 -0500

    finished styling infocard

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -10,7 +10,7 @@ const InfoCard = props => {
   // )
 
   return (
-    <div className="card question">
+    <div className="question">
       <div>{question.description}</div>
       {question.snippet ? (
         <Highlight language="javascript">{question.snippet}</Highlight>
@@ -18,13 +18,22 @@ const InfoCard = props => {
         ''
       )}
       <div style={{ visibility: props.visibility }}>
-        <InfoFeedback reveal={question.reveal} />
+        {question.reveal ? (
+          <InfoFeedback reveal={question.reveal} />
+        ) : (
+          <div className="reveal-info">
+            You're one step closer to becoming a top notch programmer!
+          </div>
+        )}
       </div>
-      {props.visibility === 'hidden' ? (
-        <button onClick={props.handleCheckAnswer}>Run</button>
-      ) : (
-        <button onClick={props.handleSubmit}>Continue</button>
-      )}
+      <div className="border-top" />
+      <div className="question-submit">
+        {props.visibility === 'hidden' ? (
+          <button onClick={props.handleCheckAnswer}>Run</button>
+        ) : (
+          <button onClick={props.handleSubmit}>Continue</button>
+        )}
+      </div>
     </div>
   );
 };
