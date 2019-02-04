dc4f8fabecac18e50ea1555916283c7601841ff5
rhianna larocque <rhiannamrl@gmail.com>
2019-02-04 16:12:53 -0500

    test

--- src/store/index.spec.js
+++ src/store/index.spec.js
@@ -1,49 +1,65 @@
-'use strict';
+import { rootReducer } from './index'
 
-import { rootReducer } from './index';
-// import getLevelQuestions from './index';
-
-// Assertions
-const chai = require('chai');
-const expect = chai.expect;
-
-// Redux
+const chai = require('chai')
+const expect = chai.expect
 
 const dataFromFirebase = {
   question1: {
     answer1: {
       isCorrect: true,
-      val: 'let x = "banana"',
+      val: 'let x = "banana"'
     },
     answer2: {
       isCorrect: false,
-      val: 'let x = banana',
+      val: 'let x = banana'
     },
     answer3: {
       isCorrect: false,
-      val: 'let x = true',
+      val: 'let x = true'
     },
     description: 'Which of the following is an example of a string?',
     id: 1,
-    type: 'multipleChoice',
+    type: 'multipleChoice'
   },
   question2: {
     answer1: {
       isCorrect: false,
-      val: 5,
+      val: 5
     },
     answer2: {
       isCorrect: false,
-      val: 7,
+      val: 7
     },
     answer3: {
       isCorrect: true,
-      val: 9,
+      val: 9
     },
     description: '"What does this program output? \tconsole.log(3 * (2+1))"',
-    id: 2,
-  },
-};
+    id: 2
+  }
+}
+
+const userDataFromFirebase = {
+  currentLevel: 2,
+  username: '@mcrae'
+}
+
+const firebaseLeaderboard = [
+  {
+    user1: {
+      score: 10,
+      username: '@camryn'
+    },
+    user2: {
+      score: 20,
+      username: '@rhianna'
+    },
+    user3: {
+      score: 400,
+      username: '@amanda'
+    }
+  }
+]
 
 describe('Store', () => {
   describe('Store / Redux stuff', () => {
@@ -51,44 +67,85 @@ describe('Store', () => {
       it('converts nested object to an array with 2 elements', () => {
         const result = Object.keys(dataFromFirebase)
           .map(el => dataFromFirebase[el])
-          .sort((a, b) => a.id - b.id);
+          .sort((a, b) => a.id - b.id)
 
-        expect(result).to.have.length(2);
-      });
-    });
+        expect(result).to.have.length(2)
+      })
+    })
 
     describe('Current Level Questions Reducer', () => {
       const initialState = {
         currentLevelQuestions: [],
-        currentUser: '',
-      };
+        currentUser: ''
+      }
 
       const result = Object.keys(dataFromFirebase)
         .map(el => dataFromFirebase[el])
-        .sort((a, b) => a.id - b.id);
+        .sort((a, b) => a.id - b.id)
 
       const newState = rootReducer(initialState, {
         type: 'GET_CURRENT_LEVEL_QUESTIONS',
-        questions: result,
-      });
+        questions: result
+      })
 
       it('returns a new state with questions fetched from "database" ', () => {
-        expect(newState.currentLevelQuestions).to.have.length(2);
-      });
+        expect(newState.currentLevelQuestions).to.have.length(2)
+      })
 
       it('returns a new state with the updated `current level questions`', () => {
-        // this should have changed:
-        expect(newState.currentLevelQuestions).to.deep.equal(result);
-        // this should not have changed:
-        expect(newState.currentUser).to.equal(initialState.currentUser);
-      });
+        expect(newState.currentLevelQuestions).to.deep.equal(result)
+        expect(newState.currentUser).to.equal(initialState.currentUser)
+      })
 
       it('does not modify the previous state', () => {
         expect(initialState).to.deep.equal({
           currentLevelQuestions: [],
-          currentUser: '',
-        });
-      });
-    });
-  });
-});
+          currentUser: ''
+        })
+      })
+    })
+    describe('Current Level Reducer', () => {
+      const initialState = {
+        currentLevel: ''
+      }
+      const result = userDataFromFirebase.currentLevel
+
+      const newState = rootReducer(initialState, {
+        type: 'GET_CURRENT_LEVEL',
+        level: result
+      })
+      it('returns a new state with the updated `current level`', () => {
+        expect(newState.currentLevel).to.deep.equal(result)
+      })
+
+      it('does not modify the previous state', () => {
+        expect(initialState).to.deep.equal({
+          currentLevel: ''
+        })
+      })
+    })
+    describe('Get leaderboard', () => {
+      const initialState = {
+        leaderboard: []
+      }
+      const result = firebaseLeaderboard
+
+      const newState = rootReducer(initialState, {
+        type: 'GET_LEADERBOARD',
+        board: result
+      })
+      it('returns a new state with the updated `current user`', () => {
+        expect(newState.leaderboard).to.deep.equal(result)
+      })
+      it('returns a new state with leaderboard fetched from "database" ', () => {
+        expect(newState.leaderboard).to.have.length(2)
+      })
+
+      it('does not modify the previous state', () => {
+        expect(initialState).to.deep.equal({
+          leaderboard: []
+        })
+      })
+    })
+  })
+})
