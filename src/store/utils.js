4aa4a27d1976bff4a22aa062fd29264e06ed0733
Amanda G <amagonzalez27@gmail.com>
2019-02-06 14:56:59 -0500

    remove console logs

--- src/store/utils.js
+++ src/store/utils.js
@@ -1,24 +1,13 @@
 export const isYesterday = date => {
   const objectDate = JSON.parse(date);
-  console.log('we jasonified our date', objectDate);
 
   const dateFromDB = new Date(objectDate);
-  console.log('date from DB', dateFromDB);
-
   const dbMonth = dateFromDB.getMonth();
-  console.log('dbmonth', dbMonth);
   const dbDay = dateFromDB.getDate();
-  console.log('dbDay', dbDay);
 
   const today = new Date();
-  console.log('today', today);
-
-  console.log(today);
   const todayMonth = today.getMonth();
-  console.log('todayMonth', todayMonth);
-
   const todayDay = today.getDate();
-  console.log('todayDay', todayDay);
 
   if (dbMonth === todayMonth && dbDay === todayDay - 1) {
     return true;
@@ -29,25 +18,14 @@ export const isYesterday = date => {
 
 export const isToday = date => {
   const objectDate = JSON.parse(date);
-  console.log('we jasonified our date', objectDate);
 
   const dateFromDB = new Date(objectDate);
-  console.log('date from DB', dateFromDB);
-
   const dbMonth = dateFromDB.getMonth();
-  console.log('dbmonth', dbMonth);
   const dbDay = dateFromDB.getDate();
-  console.log('dbDay', dbDay);
 
   const today = new Date();
-  console.log('today', today);
-
-  console.log(today);
   const todayMonth = today.getMonth();
-  console.log('todayMonth', todayMonth);
-
   const todayDay = today.getDate();
-  console.log('todayDay', todayDay);
 
   if (dbMonth === todayMonth && dbDay === todayDay) {
     return true;
