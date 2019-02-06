d775b23d12ef434c4de4f7e7c4cf471541661846
rhianna larocque <rhiannamrl@gmail.com>
2019-02-06 11:17:14 -0500

    update

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from 'react'
 
 export const Feedback = ({ answer }) => {
   return (
     <div>
       {answer.isCorrect ? (
         <div id="green-feedback">
-          <h3>Great Job! You are Correct!</h3>
+          <h3>Great Job!</h3>
           <p>{answer.val} is the correct answer!!</p>
         </div>
       ) : (
         <div id="red-feedback">
-          <h3>Oops, not quite.</h3>
+          <h3>Oops!</h3>
           <p>Try again next time!</p>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
